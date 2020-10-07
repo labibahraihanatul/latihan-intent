5a73afb3839516ed4e28b28c03005b065672af69
3103119100@student.smktelkom-pwt.sch.id <3103119100@student.smktelkom-pwt.sch.id>
2020-10-07 17:28:10 +0700

    latihan intent

---
+++ app/src/main/java/com/labibah/myintentapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.labibah.myintentapp
+
+import android.os.Bundle
+import android.widget.Button
+
+override fun onCreate(savedIntanceState: Bundle?) {
+    super.onCreate(savedIntanceState)
+    setContentView(R.layout.activity_main)
+
+    val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+}
